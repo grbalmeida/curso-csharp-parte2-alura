90e8174789729da0b6d4a7d531afd5245acd9b0e
gilvan <g.r.almeida@live.com>
2020-08-16 20:30:57 -0300

    Construtores

---
+++ ByteBank/07-ByteBank/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(867, 86712540);
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
