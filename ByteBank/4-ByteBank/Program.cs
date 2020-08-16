e705fdab962c33671a58bf6ca18c9d851212f82b
gilvan <g.r.almeida@live.com>
2020-08-15 22:15:54 -0300

    Nossa primeira função

---
+++ ByteBank/4-ByteBank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+            
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine(contaDoBruno.saldo);
+            bool resultadoSaque = contaDoBruno.Sacar(500);
+            Console.WriteLine(contaDoBruno.saldo);
+            Console.WriteLine(resultadoSaque);
+
+            Console.ReadLine();
+        }
+    }
+}
